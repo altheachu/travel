e9d420ea36a0cdc9b228452e3b13c866cfb93a4a
cch <aehtla2012@gmail.com>
2025-04-27 23:27:12 +0800

    [FIX] close Java SSL check temporarily for RestTemplate application

--- src/main/java/ecommerce/travel/utility/service/impl/UtilityServiceImpl.java
+++ src/main/java/ecommerce/travel/utility/service/impl/UtilityServiceImpl.java
@@ -1,27 +1,57 @@
 package ecommerce.travel.utility.service.impl;
 
 import ecommerce.travel.utility.dto.weather.WeatherHazardItemProxyDTO;
+import ecommerce.travel.utility.dto.weather.WeatherHazardLocationProxyDTO;
 import ecommerce.travel.utility.dto.weather.WeatherRecordProxyDTO;
 import ecommerce.travel.utility.dto.weather.WeatherRptProxyDTO;
 import ecommerce.travel.utility.service.UtilityService;
 import ecommerce.travel.utility.utils.WeatherConstant;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 import java.util.*;
 
 @Service
 public class UtilityServiceImpl implements UtilityService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Value("${weather.rpt.key}")
+    private String weatherApiKey;
+
+    private RestTemplate createUnsafeRestTemplate() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    }
+            };
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(org.apache.http.impl.client.HttpClients.custom()
+                    .setSSLContext(sslContext)
+                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                    .build());
+            return new RestTemplate(requestFactory);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public List<String> findWeatherHazardIn24Hours() throws Exception{
         final String url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/W-C0033-002?Authorization={authorization}";
         Map<String, String> paramMap = new HashMap<>();
         paramMap.put("authorization", "CWA-6F4180A1-F987-41D2-90BF-593FB8E407CA");
-        WeatherRptProxyDTO res = restTemplate.getForObject(url, WeatherRptProxyDTO.class, paramMap);
+        WeatherRptProxyDTO res = createUnsafeRestTemplate().getForObject(url, WeatherRptProxyDTO.class, paramMap);
         if(isHarzardQuerySuccess(res)){
             return formatHazardData(res.getRecords().getRecord());
         }
@@ -44,10 +74,13 @@ public class UtilityServiceImpl implements UtilityService {
             String resultStr = "";
             for(WeatherHazardItemProxyDTO info : hazard){
                 StringBuilder sb = new StringBuilder("");
-                sb.append(WeatherConstant.phenomenaBilinqualMap.get(info.getInfo().getPhenomena()));
-                sb.append(info.getInfo().getSignificance() + "!!! ");
-                sb.append(info.getInfo().getAffectedAreas());
-                resultStr = sb.toString();
+                sb.append(WeatherConstant.phenomenaBilinqualMap.getOrDefault(info.getInfo().getPhenomena(), info.getInfo().getPhenomena()));
+                sb.append(WeatherConstant.significanceBilinqualMap.getOrDefault(info.getInfo().getSignificance(), info.getInfo().getSignificance()) + "! ");
+                for(WeatherHazardLocationProxyDTO location : info.getInfo().getAffectedAreas().getLocation()){
+                    sb.append(WeatherConstant.locationBilinqualMap.getOrDefault(location.getLocationName(), location.getLocationName()));
+                    sb.append(", ");
+                }
+                resultStr = sb.substring(0, sb.length() - 2);
             }
             resultStrs.add(resultStr);
         }
--- src/main/java/ecommerce/travel/utility/utils/WeatherConstant.java
+++ src/main/java/ecommerce/travel/utility/utils/WeatherConstant.java
@@ -7,12 +7,46 @@ public class WeatherConstant {
 
     public static Map<String, String> phenomenaBilinqualMap = Map.of(
             "濃霧","Heavy Fog",
-            "大雨","Rain",
-            "豪雨","Rain",
-            "大豪雨","Rain",
-            "超大豪雨","Rain",
+            "大雨","Heavy Rain",
+            "豪雨","Extremely Heavy Rain",
+            "大豪雨","Torrential Rain",
+            "超大豪雨","Extremely Torrential Rain",
             "陸上強風","Strong Wind",
             "颱風","Typhoon"
     );
 
+    public static Map<String, String> significanceBilinqualMap = Map.of(
+            "警報","Warning",
+            "特報","Advisory"
+    );
+
+    public static Map<String, String> locationBilinqualMap;
+
+    static {
+        locationBilinqualMap = new HashMap<>();
+        locationBilinqualMap.put("臺北市","Taipei City");
+        locationBilinqualMap.put("新北市","New Taipei City");
+        locationBilinqualMap.put("桃園市","Taoyuan City");
+        locationBilinqualMap.put("臺中市","Taichung City");
+        locationBilinqualMap.put("臺南市","Tainan City");
+        locationBilinqualMap.put("高雄市","Kaohsiung City");
+        locationBilinqualMap.put("基隆市","Keelung City");
+        locationBilinqualMap.put("新竹市","Hsinchu City");
+        locationBilinqualMap.put("嘉義市","Chiayi City");
+        locationBilinqualMap.put("新竹縣","Hsinchu County");
+        locationBilinqualMap.put("苗栗縣","Miaoli County");
+        locationBilinqualMap.put("彰化縣","Changhua County");
+        locationBilinqualMap.put("南投縣","Nantou County");
+        locationBilinqualMap.put("雲林縣","Yunlin County");
+        locationBilinqualMap.put("嘉義縣","Chiayi County");
+        locationBilinqualMap.put("屏東縣","Pingtung County");
+        locationBilinqualMap.put("宜蘭縣","Yilan County");
+        locationBilinqualMap.put("花蓮縣","Hualien County");
+        locationBilinqualMap.put("臺東縣","Taitung County");
+        locationBilinqualMap.put("澎湖縣","Penghu County");
+        locationBilinqualMap.put("金門縣","Kinmen County");
+        locationBilinqualMap.put("連江縣","Lienchiang County");
+        locationBilinqualMap.put("恆春半島","Hengchun Peninsula");
+        locationBilinqualMap.put("蘭嶼綠島","Orchid Island, Green Island");
+    }
 }
